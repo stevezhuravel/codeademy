d0a5378056f6ac1334f55f94c93b5015b84f571d
Stevezhuravel <Stevezhuravel@gmail.com>
2020-07-05 17:34:17 -0400

    updated files adding comments

--- secretmesssages.js
+++ secretmesssages.js
@@ -3,9 +3,12 @@ const message = ['College is good', 'learning javascript' , 'codeacademy is help
 
 function popLastElement(arr){
     arr.pop()
+    // checking to see if you can pop from array more than once
+    arr.pop()
 
 }
 // pass an array to the method
+popLastElement(message);
 popLastElement(message);
 //  log it to the screen to see that it popped the last element 
 console.log(message)
@@ -25,6 +28,7 @@ console.log(message.length);
 
 // slice start from the position you tell it to and end position is minus one         
 console.log(message.slice(1,3));
+console.log(message.slice(2,4));
 
     // removes the first element and print the array
       const moveElement = message.shift();
@@ -33,5 +37,6 @@ console.log(message + moveElement);
 // unshift you do not have to set it to a variable
 // you just call the array name in shift you have to set it to a variale 
 // adds it to the last elements
+// it shift everything by one
 message.unshift('popcorn');
 console.log(message);
